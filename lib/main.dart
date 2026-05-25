319553d9bead1243b4506361af882963b675a542
Nguyễn Minh Tuệ <nguyenminhtue29@gmail.com>
2026-05-26 03:45:26 +0700

    add-firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+//Import thư viện Firebase.
 import 'package:firebase_core/firebase_core.dart';
+//Import file cấu hình mà FlutterFire tự tạo.
 import 'firebase_options.dart';
 
 void main() async {
-
+  //Đảm bảo Flutter khởi tạo xong trước khi dùng Firebase.
   WidgetsFlutterBinding.ensureInitialized();
 
+  //Dòng này kết nối app với Firebase.
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
