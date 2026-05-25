286fa95ef1f7f76a646e4a82582fddfda383c8b2
Nguyễn Minh Tuệ <nguyenminhtue29@gmail.com>
2026-05-26 03:41:39 +0700

    add-firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
@@ -9,8 +19,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
+          title: const Text("Firebase Demo"),
         ),
+        body: const Center(
+          child: Text("Firebase Connected"),
         ),
       ),
     );
